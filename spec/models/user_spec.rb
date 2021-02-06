3eda12daa8a384e5dfdde3b10105619434ba3d84
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-06 16:32:58 +0900

    ユーザー管理機能モデル単体テスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+    context '新規登録成功' do
+      it 'name/email/password/password_confirmationが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録失敗' do
+      it 'nameが空だと登録できない' do
+        @user.name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'emailが空だと登録できない' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'emailに@がないと登録できない' do
+        @user.email = 'test'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it '同じemailが既にデータベースにあると登録できない' do
+        @user_first = FactoryBot.create(:user)
+        @user.email = @user_first.email
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'passwordが空だと登録できない' do
+        @user.password = nil
+        @user.password_confirmation = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'password_confirmationが空だと登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordとpassword_confirmationが不一致だと登録できない' do
+        @user.password = "111111"
+        @user.password_confirmation = "111112"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
+end
