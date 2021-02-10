b29af57b78d1c9fc48af699d304519817529ee1b
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-10 21:06:14 +0900

    コントローラー

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,26 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    binding.pry
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:room_name, :player_number, :password, :password_confirmation).merge(user_id: current_user.id)
   end
 end
