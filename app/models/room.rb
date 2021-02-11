97dfd296ef1aa39e2c19dee54c5563b345b6a995
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-11 12:45:00 +0900

    アソシエーション修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,10 +5,10 @@ before_create :create_token
 
   with_options presence: true do
     validates :room_name
-    validates :player_number, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 6 }
+    validates :player_number, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 6 }
   end
   validates :password, length: { minimum: 4 }
-  has_many :user
+  belongs_to :user
   has_secure_password
 
   private
