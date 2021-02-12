27e08aab48b4e2be67365f34d30405097561cdae
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-12 21:32:51 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'rooms#index'
-  resources :rooms, only: [:new, :create, :show], param: :token
+  resources :rooms, only: [:new, :create, :show], param: :token do
+    resources :characters, only: [:new, :create]
+    resources :conditions, only: [:new, :create]
+    resources :feelings, only: [:new, :create]
+  end
 end
