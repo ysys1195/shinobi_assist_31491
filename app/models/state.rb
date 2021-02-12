07f1bf994f3fbd08b526857bd4ac1602c27d0794
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-12 21:11:18 +0900

    ActiveHash conditionテーブル修正

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,18 +1,18 @@
 class State < ActiveHash::Base
   self.data = [
-    { id: 1, name: "感情を選択" }
-    { id: 2, name: "共感(プラス)" }
-    { id: 3, name: "不信(マイナス)" }
-    { id: 4, name: "友情(プラス)" }
-    { id: 5, name: "怒り(マイナス)" }
-    { id: 6, name: "愛情(プラス)" }
-    { id: 7, name: "妬み(マイナス)" }
-    { id: 8, name: "忠誠(プラス)" }
-    { id: 9, name: "侮蔑(マイナス)" }
-    { id: 10, name: "憧憬(プラス)" }
-    { id: 11, name: "劣等感(マイナス)" }
-    { id: 12, name: "狂信(プラス)" }
-    { id: 13, name: "殺意(マイナス)" }
+    { id: 1, name: "変調を選択" }
+    { id: 2, name: "故障" }
+    { id: 3, name: "マヒ" }
+    { id: 4, name: "重傷" }
+    { id: 5, name: "行方不明" }
+    { id: 6, name: "忘却" }
+    { id: 7, name: "呪い" }
+    { id: 8, name: "催眠" }
+    { id: 9, name: "火達磨" }
+    { id: 10, name: "猛毒" }
+    { id: 11, name: "飢餓" }
+    { id: 12, name: "残刃" }
+    { id: 13, name: "野望" }
   ]
 
   include ActiveHash::Associations
