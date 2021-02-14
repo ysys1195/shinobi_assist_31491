2d225a44291df2b2d1716c127c03955445b4f325
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-14 17:55:34 +0900

    セッションルームコントローラー

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,6 +4,17 @@ class CharactersController < ApplicationController
   end
 
   def create
-    
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to root_path
+    else
+      render: :new
+    end
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:character_name, :secret, :last_resort)
   end
 end
