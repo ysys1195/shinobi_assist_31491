794a5b06d7313a72583e39ed7a3832b59636b689
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-19 21:48:38 +0900

    Revert "ルーティング変更　newアクション削除"
    This reverts commit 093a154b6daef4346cf185becd1dda524966096e.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'rooms#index'
   resources :rooms, only: [:create, :show], param: :token do
-    resources :characters, only: [:create]
-    resources :conditions, only: [:create]
-    resources :feelings, only: [:create]
+    resources :characters, only: [:new, :create]
+    resources :conditions, only: [:new, :create]
+    resources :feelings, only: [:new, :create]
   end
 end
