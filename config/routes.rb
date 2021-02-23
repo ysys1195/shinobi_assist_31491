12dd78c3da2a06988f43997ed0905739fe22238c
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-23 15:12:22 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'rooms#index'
   resources :rooms, only: [:create, :show], param: :token do
     resources :characters, only: [:create, :update]
-    resources :conditions, only: [:new, :create]
+    resources :conditions, only: [:create]
     resources :feelings, only: [:new, :create]
   end
 end
