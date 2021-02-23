27f0bbdcb579d90650b5b65781c16f13177e1d8b
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-23 15:13:42 +0900

    変調情報入力用コントローラー

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,2 +1,14 @@
 class ConditionsController < ApplicationController
+  def create
+    @condition = Condition.new(condition_params)
+    if @condition.save
+      redirect_to room_path(token: params[:room_token])
+    end
+  end
+
+  private
+  
+  def condition_params
+    params.require(:condition).permit(:state_id, :character_id)
+  end
 end
