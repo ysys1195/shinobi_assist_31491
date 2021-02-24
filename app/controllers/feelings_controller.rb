84a737975c17a509cf3a800ef5e41c6e865c907a
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-24 14:56:26 +0900

    感情入力　コントローラー

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,2 +1,14 @@
 class FeelingsController < ApplicationController
+  def create
+    @feeling = Feeling.new(feeling_params)
+    return unless @feeling.save
+
+    redirect_to room_path(token: params[:room_token])
+  end
+
+  private
+
+  def feeling_params
+    params.require(:feeling).permit(:feel_id, :feeling_to, :character_id)
+  end
 end
