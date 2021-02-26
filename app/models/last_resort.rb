e726f843bec63eccb86f8c526f7b4792ddb0eddb
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-26 11:04:51 +0900

    奥義系モデル変更

--- app/models/last_resort.rb
+++ app/models/last_resort.rb
@@ -1,2 +1,9 @@
 class LastResort < ApplicationRecord
+  with_options presence: true do
+    validates :last_resort_n
+    validates :last_resort
+  end
+  
+  belongs_to :character
+  has_many :open_last_resorts
 end
