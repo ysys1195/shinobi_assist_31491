b98261c09a52bb7ada8d9620cdc056353c7da8ad
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-26 11:15:08 +0900

    奥義・秘密系ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'rooms#index'
   resources :rooms, only: [:create, :show, :destroy], param: :token do
     resources :characters, only: [:create, :update]
+    resources :secrets, only: [:create]
+    resources :open_secrets, only: [:create]
+    resources :last_resorts, only: [:create]
+    resources :open_last_resorts, only: [:create]
     resources :conditions, only: [:create, :destroy]
     resources :feelings, only: [:create, :destroy]
   end
