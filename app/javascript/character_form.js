3a489a3fb5e55ee63c285e06078918a303c576b6
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-27 19:40:21 +0900

    エラー表示変更

--- app/javascript/character_form.js
+++ app/javascript/character_form.js
@@ -1,6 +1,6 @@
 const charForm = $(() => {
   // PC情報入力ボタン / PC情報編集ボタン/ 感情と変調の追加ボタン のクリック時
-  $('.input_char_info, .char-edit-btn, .click_frame').each(function() {
+  $('.input_char_info, .click_frame, .character_name, .add-charinfo-btn').each(function() {
     $(this).on('click', function() {
       const target = $(this).data('target');
       const modal = document.getElementById(target);
@@ -13,16 +13,6 @@ const charForm = $(() => {
     return false;
   });
   // PC情報入力ボタン / PC情報編集ボタン/ 感情と変調の追加ボタン のクリック時
-
-  // エラー表示
-  $('.add-btn').each(function() {
-    $(this).on('click', function() {
-      const target = $(this).data('target');
-      const hiddenError = document.getElementById(target);
-      $(hiddenError).css({ display: 'inline-block' });
-    });
-  });
-  // エラー表示
 
   // マウスオーバー時
   $('.status_over_frame, .status_over_frame2').each(function() {
