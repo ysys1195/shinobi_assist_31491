de835a935574550194b79d39a57b2c98eb6de11e
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-02-27 18:50:43 +0900

    奥義　モデル追加

--- app/models/last_resort.rb
+++ app/models/last_resort.rb
@@ -1,4 +1,8 @@
 class LastResort < ApplicationRecord
+  with_options presence: true do
+    validates :last_resort_n
+    validates :last_resort
+  end
   
   belongs_to :character
   has_many :open_last_resorts
