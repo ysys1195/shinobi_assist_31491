e54c61f06d878278448e9fc17931b7c3fdbac6a7
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-01 15:24:01 +0900

    秘密エラー表示変更

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -6,6 +6,7 @@ class SecretsController < ApplicationController
     else
       flash[:error] = @add_secret.errors.full_messages
       flash[:num] = params[:secret][:pc_number].to_i
+      flash[:match] = "secret#{flash[:num]}"
       flash[:error_msg] = "PC#{flash[:num]}の秘密を更新できませんでした。再度入力をお願いします。"
       redirect_to room_path(token: params[:room_token])
     end
