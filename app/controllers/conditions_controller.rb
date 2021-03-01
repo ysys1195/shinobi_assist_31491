fe96d4b731eb7318d273c0f1389abfb0d2c286fa
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-01 15:23:17 +0900

    変調エラー表示変更

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -6,6 +6,7 @@ class ConditionsController < ApplicationController
     else
       flash[:error] = @condition.errors.full_messages
       flash[:num] = params[:condition][:pc_number].to_i
+      flash[:match] = "condition#{flash[:num]}"
       flash[:error_msg] = "PC#{flash[:num]}の変調を更新できませんでした。再度選択をお願いします。"
       redirect_to room_path(token: params[:room_token])
     end
