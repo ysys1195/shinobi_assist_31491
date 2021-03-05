c11ec11e463f16ceee69aae285346fc8b912d74b
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-05 22:51:11 +0900

    秘密系ビュー変更1

--- app/javascript/character_form.js
+++ app/javascript/character_form.js
@@ -8,6 +8,14 @@ const charForm = $(() => {
       return false;
     });
   });
+  $('.edit-charinfo-btn').each(function() {
+    $(this).on('click', function() {
+      const target = $(this).data('target');
+      const modal = document.getElementById(target);
+      $(modal).fadeIn(300);
+      return false;
+    });
+  });
   $('.form-background').on('click', function() {
     $('.wrap-char-form').fadeOut(300);
     return false;
