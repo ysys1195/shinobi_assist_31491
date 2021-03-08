ef8a2ab437c5cac410aea6e66d6be8d64ce5ba23
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-08 15:40:14 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
     resources :secrets, only: [:create, :update]
     resources :open_secrets, only: [:update]
     get 'open_secrets/:id', to: 'open_secrets#unveiled'
-    resources :last_resorts, only: [:create]
-    resources :open_last_resorts, only: [:create]
+    resources :last_resorts, only: [:create, :update]
+    resources :open_last_resorts, only: [:update]
+    get 'open_last_resorts/:id', to: 'open_last_resorts#unveiled'
     resources :conditions, only: [:create, :destroy]
     resources :feelings, only: [:create, :destroy]
   end
