82d144fecb665dc1a52f3ca55294a7921f4cf375
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-13 11:37:51 +0900

    公開非公開の修正

--- app/controllers/last_resorts_controller.rb
+++ app/controllers/last_resorts_controller.rb
@@ -1,13 +1,19 @@
 class LastResortsController < ApplicationController
   def create
     @add_last_resort = LastResort.new(last_resort_params)
-    unless @add_last_resort.save
+    if @add_last_resort.save
+      character = Character.find(@add_last_resort.character_id).pc_number
+      room = Room.find_by(token: params[:room_token])
+      room.player_number.times do |n|
+        OpenLastResort.create(lr_unveil_to: n + 1, unveiled_id: 1, last_resort_id: @add_last_resort.id) unless character == n + 1
+      end
+    else
       flash[:error] = @add_last_resort.errors.full_messages
       flash[:num] = params[:last_resort][:pc_number].to_i
       flash[:match] = "lr#{flash[:num]}"
       flash[:error_msg] = "PC#{flash[:num]}の奥義を更新できませんでした。再度入力をお願いします。"
     end
-    redirect_to room_path(token: params[:room_token])
+    redirect_to room_path(token: room.token)
   end
 
   def update
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,19 @@
 class SecretsController < ApplicationController
   def create
     @add_secret = Secret.new(secret_params)
-    unless @add_secret.save
+    if @add_secret.save
+      character = Character.find(@add_secret.character_id).pc_number
+      room = Room.find_by(token: params[:room_token])
+      room.player_number.times do |n|
+        OpenSecret.create(secret_unveil_to: n + 1, unveiled_id: 1, secret_id: @add_secret.id) unless character == n + 1
+      end
+    else
       flash[:error] = @add_secret.errors.full_messages
       flash[:num] = params[:secret][:pc_number].to_i
       flash[:match] = "secret#{flash[:num]}"
       flash[:error_msg] = "PC#{flash[:num]}の秘密を更新できませんでした。再度入力をお願いします。"
     end
-    redirect_to room_path(token: params[:room_token])
+    redirect_to room_path(token: room.token)
   end
 
   def update
