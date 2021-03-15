c343ea0757f07a78239aa3174d2df26f36c123cc
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-15 22:48:55 +0900

    秘密追加の非同期

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,19 +1,27 @@
 class SecretsController < ApplicationController
   def create
     @add_secret = Secret.new(secret_params)
-    if @add_secret.save
-      character = Character.find(@add_secret.character_id).pc_number
-      room = Room.find_by(token: params[:room_token])
-      room.player_number.times do |n|
-        OpenSecret.create(secret_unveil_to: n + 1, unveiled_id: 1, secret_id: @add_secret.id) unless character == n + 1
+    respond_to do |format|
+      if @add_secret.save
+        character = Character.find(@add_secret.character_id).pc_number
+        @room = Room.find_by(token: params[:room_token])
+        @room.player_number.times do |n|
+          OpenSecret.create(secret_unveil_to: n + 1, unveiled_id: 1, secret_id: @add_secret.id) unless character == n + 1
+        end
+        @open_secrets = OpenSecret.where(secret_id: @add_secret.id)
+        @role = Role.find_by(user_id: current_user.id, room_id: @room.id).role
+        @secret = { num: character.to_i, selected_char: Character.find(@add_secret.character_id) }
+        @characters = Character.where(room_id: @room.id)
+        format.js { render 'secrets/add_secret' }
+      else
+        flash[:error] = @add_secret.errors.full_messages
+        flash[:num] = params[:secret][:pc_number].to_i
+        flash[:match] = "secret#{flash[:num]}"
+        flash[:error_msg] = "PC#{flash[:num]}の秘密を更新できませんでした。再度入力をお願いします。"
+        format.js { render 'shared/errors' }
       end
-    else
-      flash[:error] = @add_secret.errors.full_messages
-      flash[:num] = params[:secret][:pc_number].to_i
-      flash[:match] = "secret#{flash[:num]}"
-      flash[:error_msg] = "PC#{flash[:num]}の秘密を更新できませんでした。再度入力をお願いします。"
+      format.html { redirect_to room_path(token: params[:room_token]) }
     end
-    redirect_to room_path(token: room.token)
   end
 
   def update
