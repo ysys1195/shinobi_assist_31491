db4e4f3b032fa752c1e8ce95aa52620388ee177f
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-15 23:40:14 +0900

    奥義追加非同期

--- app/controllers/last_resorts_controller.rb
+++ app/controllers/last_resorts_controller.rb
@@ -1,19 +1,28 @@
 class LastResortsController < ApplicationController
+  before_action :find_room, only: [:create, :update]
+  
   def create
     @add_last_resort = LastResort.new(last_resort_params)
-    if @add_last_resort.save
-      character = Character.find(@add_last_resort.character_id).pc_number
-      room = Room.find_by(token: params[:room_token])
-      room.player_number.times do |n|
-        OpenLastResort.create(lr_unveil_to: n + 1, unveiled_id: 1, last_resort_id: @add_last_resort.id) unless character == n + 1
+    respond_to do |format|
+      if @add_last_resort.save
+        character = Character.find(@add_last_resort.character_id).pc_number
+        @room.player_number.times do |n|
+          OpenLastResort.create(lr_unveil_to: n + 1, unveiled_id: 1, last_resort_id: @add_last_resort.id) unless character == n + 1
+        end
+        @open_last_resorts = OpenLastResort.where(last_resort_id: @add_last_resort.id)
+        @role = Role.find_by(user_id: current_user.id, room_id: @room.id).role
+        @lr = { num: character, selected_char: Character.find(@add_last_resort.character_id) }
+        @characters = Character.where(room_id: @room.id)
+        format.js { render 'last_resorts/add_last_resort' }
+      else
+        flash[:error] = @add_last_resort.errors.full_messages
+        flash[:num] = params[:last_resort][:pc_number].to_i
+        flash[:match] = "lr#{flash[:num]}"
+        flash[:error_msg] = "PC#{flash[:num]}の奥義を更新できませんでした。再度入力をお願いします。"
+        format.js { render 'shared/errors' }
       end
-    else
-      flash[:error] = @add_last_resort.errors.full_messages
-      flash[:num] = params[:last_resort][:pc_number].to_i
-      flash[:match] = "lr#{flash[:num]}"
-      flash[:error_msg] = "PC#{flash[:num]}の奥義を更新できませんでした。再度入力をお願いします。"
+      format.html { redirect_to room_path(token: @room.token) }
     end
-    redirect_to room_path(token: room.token)
   end
 
   def update
@@ -24,12 +33,16 @@ class LastResortsController < ApplicationController
       flash[:match] = "lr#{flash[:num]}"
       flash[:error_msg] = "PC#{flash[:num]}の奥義を更新できませんでした。再度入力をお願いします。"
     end
-    redirect_to room_path(token: params[:room_token])
+    redirect_to room_path(token: @room.token)
   end
 
   private
 
   def last_resort_params
     params.require(:last_resort).permit(:last_resort_n, :last_resort, :character_id)
+  end
+
+  def find_room
+    @room = Room.find_by(token: params[:room_token])
   end
 end
