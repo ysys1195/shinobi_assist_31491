0b57ceb98d652ffff1c73b0398e193dd7ec25be0
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-16 01:24:41 +0900

    変調非同期通信

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,13 +1,20 @@
 class ConditionsController < ApplicationController
   def create
     @condition = Condition.new(condition_params)
-    unless @condition.save
-      flash[:error] = @condition.errors.full_messages
-      flash[:num] = params[:condition][:pc_number].to_i
-      flash[:match] = "condition#{flash[:num]}"
-      flash[:error_msg] = "PC#{flash[:num]}の変調を更新できませんでした。再度選択をお願いします。"
+    respond_to do |format|
+      if @condition.save
+        @pc_num = Character.find(@condition.character_id).pc_number
+        @room = Room.find_by(token: params[:room_token])
+        format.js { render 'conditions/add_condition' }
+      else
+        flash[:error] = @condition.errors.full_messages
+        flash[:num] = params[:condition][:pc_number].to_i
+        flash[:match] = "condition#{flash[:num]}"
+        flash[:error_msg] = "PC#{flash[:num]}の変調を更新できませんでした。再度選択をお願いします。"
+        format.js { render 'shared/errors' }
+      end
+      format.html { redirect_to room_path(token: params[:room_token]) }
     end
-    redirect_to room_path(token: params[:room_token])
   end
 
   def destroy
