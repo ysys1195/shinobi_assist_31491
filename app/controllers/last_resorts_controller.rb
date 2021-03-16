937f6b8dcc2c2553ef605e30fb2194d38685b4e8
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-16 19:50:51 +0900

    rubocop

--- app/controllers/last_resorts_controller.rb
+++ app/controllers/last_resorts_controller.rb
@@ -1,24 +1,20 @@
 class LastResortsController < ApplicationController
   before_action :find_room, only: [:create, :update]
-  
+
   def create
     @add_last_resort = LastResort.new(last_resort_params)
     respond_to do |format|
       if @add_last_resort.save
-        character = Character.find(@add_last_resort.character_id).pc_number
+        character = Character.find(@add_last_resort.character_id)
         @room.player_number.times do |n|
-          OpenLastResort.create(lr_unveil_to: n + 1, unveiled_id: 1, last_resort_id: @add_last_resort.id) unless character == n + 1
+          if character.pc_number != n + 1
+            OpenLastResort.create(lr_unveil_to: n + 1, unveiled_id: 1, last_resort_id: @add_last_resort.id)
+          end
         end
-        @open_last_resorts = OpenLastResort.where(last_resort_id: @add_last_resort.id)
-        @role = Role.find_by(user_id: current_user.id, room_id: @room.id).role
-        @lr = { num: character, selected_char: Character.find(@add_last_resort.character_id) }
-        @characters = Character.where(room_id: @room.id)
+        definition_create(character)
         format.js { render 'last_resorts/add_last_resort' }
       else
-        flash[:error] = @add_last_resort.errors.full_messages
-        flash[:num] = params[:last_resort][:pc_number].to_i
-        flash[:match] = "lr#{flash[:num]}"
-        flash[:error_msg] = "PC#{flash[:num]}の奥義を更新できませんでした。再度入力をお願いします。"
+        error_message(params[:last_resort][:pc_number], @add_last_resort)
         format.js { render 'shared/errors' }
       end
       format.html { redirect_to room_path(token: @room.token) }
@@ -32,10 +28,7 @@ class LastResortsController < ApplicationController
         @pc_num = Character.find(@edit_last_resort.character_id).pc_number
         format.js { render 'last_resorts/edit_last_resort' }
       else
-        flash[:error] = @edit_last_resort.errors.full_messages
-        flash[:num] = params[:last_resort][:pc_number].to_i
-        flash[:match] = "lr#{flash[:num]}"
-        flash[:error_msg] = "PC#{flash[:num]}の奥義を更新できませんでした。再度入力をお願いします。"
+        error_message(params[:last_resort][:pc_number], @edit_last_resort)
         format.js { render 'shared/errors' }
       end
       format.html { redirect_to room_path(token: @room.token) }
@@ -50,5 +43,19 @@ class LastResortsController < ApplicationController
 
   def find_room
     @room = Room.find_by(token: params[:room_token])
+  end
+
+  def definition_create(character)
+    @open_last_resorts = OpenLastResort.where(last_resort_id: @add_last_resort.id)
+    @role = Role.find_by(user_id: current_user.id, room_id: @room.id).role
+    @lr = { num: character.pc_number, selected_char: character }
+    @characters = Character.where(room_id: @room.id)
+  end
+
+  def error_message(num, input)
+    flash[:error] = input.errors.full_messages
+    flash[:num] = num.to_i
+    flash[:match] = "lr#{flash[:num]}"
+    flash[:error_msg] = "PC#{flash[:num]}の奥義を更新できませんでした。再度入力をお願いします。"
   end
 end
--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -3,21 +3,15 @@ class SecretsController < ApplicationController
     @add_secret = Secret.new(secret_params)
     respond_to do |format|
       if @add_secret.save
-        character = Character.find(@add_secret.character_id).pc_number
+        character = Character.find(@add_secret.character_id)
         @room = Room.find_by(token: params[:room_token])
         @room.player_number.times do |n|
-          OpenSecret.create(secret_unveil_to: n + 1, unveiled_id: 1, secret_id: @add_secret.id) unless character == n + 1
+          OpenSecret.create(secret_unveil_to: n + 1, unveiled_id: 1, secret_id: @add_secret.id) if character.pc_number != n + 1
         end
-        @open_secrets = OpenSecret.where(secret_id: @add_secret.id)
-        @role = Role.find_by(user_id: current_user.id, room_id: @room.id).role
-        @secret = { num: character, selected_char: Character.find(@add_secret.character_id) }
-        @characters = Character.where(room_id: @room.id)
+        definition_create(character)
         format.js { render 'secrets/add_secret' }
       else
-        flash[:error] = @add_secret.errors.full_messages
-        flash[:num] = params[:secret][:pc_number].to_i
-        flash[:match] = "secret#{flash[:num]}"
-        flash[:error_msg] = "PC#{flash[:num]}の秘密を更新できませんでした。再度入力をお願いします。"
+        error_message(params[:secret][:pc_number], @add_secret)
         format.js { render 'shared/errors' }
       end
       format.html { redirect_to room_path(token: params[:room_token]) }
@@ -31,10 +25,7 @@ class SecretsController < ApplicationController
         @pc_num = Character.find(@edit_secret.character_id).pc_number
         format.js { render 'secrets/edit_secret' }
       else
-        flash[:error] = @edit_secret.errors.full_messages
-        flash[:num] = params[:secret][:pc_number].to_i
-        flash[:match] = "secret#{flash[:num]}"
-        flash[:error_msg] = "PC#{flash[:num]}の秘密を更新できませんでした。再度入力をお願いします。"
+        error_message(params[:secret][:pc_number], @edit_secret)
         format.js { render 'shared/errors' }
       end
       format.html { redirect_to room_path(token: params[:room_token]) }
@@ -45,5 +36,19 @@ class SecretsController < ApplicationController
 
   def secret_params
     params.require(:secret).permit(:secret_n, :secret, :character_id)
+  end
+
+  def definition_create(character)
+    @open_secrets = OpenSecret.where(secret_id: @add_secret.id)
+    @role = Role.find_by(user_id: current_user.id, room_id: @room.id).role
+    @secret = { num: character.pc_number, selected_char: character }
+    @characters = Character.where(room_id: @room.id)
+  end
+
+  def error_message(num, input)
+    flash[:error] = input.errors.full_messages
+    flash[:num] = num.to_i
+    flash[:match] = "secret#{flash[:num]}"
+    flash[:error_msg] = "PC#{flash[:num]}の秘密を更新できませんでした。再度入力をお願いします。"
   end
 end
