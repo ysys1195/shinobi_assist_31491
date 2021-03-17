399b030c7a3a2ef0b401c12173574a77b7c6caba
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-17 19:46:55 +0900

    last resort　モデル単体テスト

--- spec/factories/last_resorts.rb
+++ spec/factories/last_resorts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :last_resort do
-    
+    last_resort_n { Faker::Number.number(digits: 1) }
+    last_resort   { Faker::Lorem.sentence }
+    association :character
   end
 end
--- spec/models/last_resort_spec.rb
+++ spec/models/last_resort_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe LastResort, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '奥義情報の保存' do
+    before do
+      @last_resort = FactoryBot.build(:last_resort)
+    end
+
+    it '全て正しく入力したら保存できる' do
+      expect(@last_resort).to be_valid
+    end
+    it 'last_resort_nが空だと保存できない' do
+      @last_resort.last_resort_n = ''
+      @last_resort.valid?
+      expect(@last_resort.errors.full_messages).to include("Last resort n can't be blank")
+    end
+    it 'last_resortが空だと保存できない' do
+      @last_resort.last_resort = ''
+      @last_resort.valid?
+      expect(@last_resort.errors.full_messages).to include("Last resort can't be blank")
+    end
+    it 'characterと紐づいていないと保存できない' do
+      @last_resort.character = nil
+      @last_resort.valid?
+      expect(@last_resort.errors.full_messages).to include("Character must exist")
+    end
+  end
 end
