d97afc013a5925878313f9cec0e8a6ef4fdc0214
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-17 19:33:04 +0900

    open secret　モデル単体テスト

--- spec/models/open_secret_spec.rb
+++ spec/models/open_secret_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe OpenSecret, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '秘密公開か非公開の保存' do
+    before do
+      @open_secret = FactoryBot.build(:open_secret)
+    end
+
+    it 'secret_unveil_toが空だと保存できない' do
+      @open_secret.secret_unveil_to = ''
+      @open_secret.valid?
+      expect(@open_secret.errors.full_messages).to include("Secret unveil to can't be blank")
+    end
+    it 'unveiled_idが空だと保存できない' do
+      @open_secret.unveiled_id = ''
+      @open_secret.valid?
+      expect(@open_secret.errors.full_messages).to include("Unveiled can't be blank")
+    end
+    it 'unveiled_idが1か2以外だと保存できない' do
+      @open_secret.unveiled_id = 3
+      @open_secret.valid?
+      expect(@open_secret.errors.full_messages).to include("Unveiled is not included in the list")
+    end
+    it 'secretと紐づいていないと保存できない' do
+      @open_secret.secret = nil
+      @open_secret.valid?
+      expect(@open_secret.errors.full_messages).to include('Secret must exist')
+    end
+  end
 end
