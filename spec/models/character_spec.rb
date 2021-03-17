5fb625de6c6f360242c013bd8050eefb6553d108
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-17 16:46:52 +0900

    character モデル単体テスト

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Character, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'キャラクター名の保存' do
+    before do
+      @character = FactoryBot.build(:character)
+    end
+
+    it '全て正しく入力されていれば保存できる' do
+      expect(@character).to be_valid
+    end
+    it 'character_nameが空だと保存できない' do
+      @character.character_name = ''
+      @character.valid?
+      expect(@character.errors.full_messages).to include("Character name can't be blank")
+    end
+    it 'pc_numberが空だと保存できない' do
+      @character.pc_number = ''
+      @character.valid?
+      expect(@character.errors.full_messages).to include("Pc number can't be blank")
+    end
+    it 'roomと紐づいていないと保存できない' do
+      @character.room = nil
+      @character.valid?
+      expect(@character.errors.full_messages).to include('Room must exist')
+    end
+  end
 end
