81ce1e64f0f06b23eb734d72537f4e5b176d04f4
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-18 16:44:39 +0900

    role モデル単体テスト

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '役割保存' do
+    before do
+      @role = FactoryBot.build(:role)
+    end
+
+    it '全て正しく入力されていれば保存される' do
+      expect(@role).to be_valid
+    end
+    it 'userと紐づいていないと保存されない' do
+      @role.user = nil
+      @role.valid?
+      expect(@role.errors.full_messages).to include('User must exist')
+    end
+    it 'roomと紐づいていないと保存されない' do
+      @role.room = nil
+      @role.valid?
+      expect(@role.errors.full_messages).to include('Room must exist')
+    end
+  end
 end
