bc73ae351fe8bae7bed752f5e1bdaad6b23943a5
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2021-03-19 16:08:55 +0900

    モデル単体テスト

--- spec/models/session_log_spec.rb
+++ spec/models/session_log_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe SessionLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'セッション履歴の保存' do
+    before do
+      @session_log = FactoryBot.build(:session_log)
+    end
+
+    it '全て正しく入力されていれば保存される' do
+      expect(@session_log).to be_valid
+    end
+    it 'titleが空だと保存されない' do
+      @session_log.title = ''
+      @session_log.valid?
+      expect(@session_log.errors.full_messages).to include("Title can't be blank")
+    end
+    it 'logが空だと保存されない' do
+      @session_log.log = ''
+      @session_log.valid?
+      expect(@session_log.errors.full_messages).to include("Log can't be blank")
+    end
+    it 'userと紐づいていないと保存されない' do
+      @session_log.user = nil
+      @session_log.valid?
+      expect(@session_log.errors.full_messages).to include('User must exist')
+    end
+    it 'roomと紐づいていないと保存されない' do
+      @session_log.room = nil
+      @session_log.valid?
+      expect(@session_log.errors.full_messages).to include('Room must exist')
+    end
+  end
 end
